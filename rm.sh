a1fd73649de60c385335c55f8bd53e69214a4c0d
VashtiLegarreta <89096115+VashtiLegarreta@users.noreply.github.com>
2022-06-05 13:08:32 -0400

    Add files via upload

---
+++ rm.sh
@@ -0,0 +1,3 @@
+rm -rf 3proxy-3proxy-0.8.6/
+rm socks.sh
+rm rm.sh
---
+++ socks.sh
@@ -0,0 +1,20 @@
+apt-get update && apt-get -y upgrade
+apt install megatools
+apt-get install -y build-essential nano
+megadl 'https://mega.nz/#!tDllWTpZ#NFp1cWKyF4M-8H87JbKburqwffgj4HDalN65kfrTx0E'
+tar xzf socks5.tar.gz
+rm socks5.tar.gz
+cd 3proxy-3proxy-0.8.6
+make -f Makefile.Linux
+cd src
+mkdir /etc/3proxy/
+mv 3proxy /etc/3proxy/
+cd /etc/3proxy/
+wget --no-check-certificate https://raw.githubusercontent.com/VashtiLegarreta/socks/master/3proxy.cfg
+chmod 600 /etc/3proxy/3proxy.cfg
+wget --no-check-certificate https://raw.githubusercontent.com/VashtiLegarreta/socks/master/.proxyauth
+chmod 600 /etc/3proxy/.proxyauth
+cd /etc/init.d/
+wget --no-check-certificate https://raw.githubusercontent.com/VashtiLegarreta/socks/master/3proxyinit
+chmod  +x /etc/init.d/3proxyinit
+update-rc.d 3proxyinit defaults
